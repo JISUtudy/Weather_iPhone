87c674d890bc0e802bb7d9d1cf7d30230c942734
Jiseok97 <wltjr1426@naver.com>
2022-05-18 15:49:18 +0900

    [Set] 프로젝트 생성

---
+++ Weather_iPhone/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_iPhone
+//
+//  Created by 이지석 on 2022/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
