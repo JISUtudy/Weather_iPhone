783ca7060b245fb52336b567b1d98e38373f3d74
suzumsz <tnwjd6496@naver.com>
2022-05-26 13:57:14 +0900

    [Add] UILabel Extensions 추가

---
+++ Weather_iPhone/Sources/Utils/UILabel+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UILabel+Extensions.swift
+//  Weather_iPhone
+//
+//  Created by 김수정 on 2022/05/26.
+//
+
+import UIKit
+
+extension UILabel {
+    // color와 font 둘 다 변경하는 Extension
+    func setFontColor(text: String, font: UIFont, color: UIColor) {
+        self.text = text
+        self.font = font
+        self.textColor = color
+    }
+}
