301b03d8055048dac4b1cd9c7ec0775820525a09
suzumsz <tnwjd6496@naver.com>
2022-05-28 03:32:34 +0900

    [Style] tableView FooterView (#9)

--- Weather_iPhone/Sources/Controller/MainViewController.swift
+++ Weather_iPhone/Sources/Controller/MainViewController.swift
@@ -22,6 +22,53 @@ class MainViewController: UIViewController {
         return tv
     }()
     
+    lazy var footerView: UIView = {
+        let footer = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 130))
+        weatherTableView.tableFooterView = footer
+        return footer
+    }()
+    
+    lazy var footerWeatherDataBtn: UIButton = {
+        let ftWeatherDataBtn = UIButton(frame: footerView.bounds)
+        ftWeatherDataBtn.setBtnFontColor(text: "날씨 데이터", font: .systemFont(ofSize: 12), color: .white)
+        ftWeatherDataBtn.setUnderline()
+        ftWeatherDataBtn.addTarget(self, action: #selector(weatherData), for: .touchUpInside)
+        footerView.addSubview(ftWeatherDataBtn)
+        return ftWeatherDataBtn
+    }()
+    
+    lazy var footerFirstLabelBtn: UIButton = {
+        let ftFirstLabelBtn = UIButton(frame: footerView.bounds)
+        ftFirstLabelBtn.setBtnFontColor(text: "및", font: .systemFont(ofSize: 12), color: .white)
+        footerView.addSubview(ftFirstLabelBtn)
+        return ftFirstLabelBtn
+    }()
+    
+    lazy var footerMapDataBtn: UIButton = {
+        let ftMapDataBtn = UIButton(frame: footerView.bounds)
+        ftMapDataBtn.setBtnFontColor(text: "지도 데이터", font: .systemFont(ofSize: 12), color: .white)
+        ftMapDataBtn.setUnderline()
+        ftMapDataBtn.addTarget(self, action: #selector(mapData), for: .touchUpInside)
+        footerView.addSubview(ftMapDataBtn)
+        return ftMapDataBtn
+    }()
+    
+    lazy var footerLastLabelBtn: UIButton = {
+        let ftLastLabelBtn = UIButton(frame: footerView.bounds)
+        ftLastLabelBtn.setBtnFontColor(text: "에 관하여 더 알아보기", font: .systemFont(ofSize: 12), color: .white)
+        footerView.addSubview(ftLastLabelBtn)
+        return ftLastLabelBtn
+    }()
+    
+    lazy var footerImageBtn: UIButton = {
+        let ftImageBtn = UIButton(frame: footerView.bounds)
+        ftImageBtn.setImage(UIImage(named: "weather_icon"), for: .normal)
+        ftImageBtn.addTarget(self, action: #selector(weatherImage), for: .touchUpInside)
+        footerView.addSubview(ftImageBtn)
+        return ftImageBtn
+    }()
+    
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -30,16 +77,24 @@ class MainViewController: UIViewController {
         
         configureNav()
         configureUI()
+        autoLayout()
     }
-    
     
     // MARK: - Selector
     
     @objc func handleMenu() {
         print("DEBUG: Nav Right button tapped")
     }
-
-
+    @objc func weatherData() {
+        print("DEBUG: WeatehrData button tapped")
+    }
+    @objc func mapData() {
+        print("DEBUG: MapData button tapped")
+    }
+    @objc func weatherImage() {
+        print("DEBUG: WeatehrImage button tapped")
+    }
+    
     // MARK: - Functions
     
     private func configureUI() {
@@ -48,16 +103,15 @@ class MainViewController: UIViewController {
         
         view.addSubview(weatherTableView)
         
-        weatherTableView.snp.makeConstraints { make in
-            make.top.bottom.equalToSuperview()
-            make.leading.trailing.equalToSuperview().inset(24)
+        weatherTableView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(24)
         }
     }
     
     private func configureNav() {
-        
         navigationItem.title = "날씨"
-
+        
         navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.tintColor = .white
@@ -70,19 +124,42 @@ class MainViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: #selector(handleMenu))
     }
     
+    private func autoLayout() {
+        footerWeatherDataBtn.snp.makeConstraints {
+            $0.top.equalTo(footerView.snp.top)
+            $0.leading.equalTo(footerView.snp.leading).offset(70)
+        }
+        footerFirstLabelBtn.snp.makeConstraints {
+            $0.left.equalTo(footerWeatherDataBtn.snp.right).offset(-5)
+            $0.top.equalTo(footerWeatherDataBtn.snp.top)
+        }
+        footerMapDataBtn.snp.makeConstraints {
+            $0.left.equalTo(footerFirstLabelBtn.snp.right).offset(-5)
+            $0.top.equalTo(footerWeatherDataBtn.snp.top)
+        }
+        footerLastLabelBtn.snp.makeConstraints {
+            $0.left.equalTo(footerMapDataBtn.snp.right)
+            $0.top.equalTo(footerWeatherDataBtn.snp.top)
+        }
+        footerImageBtn.snp.makeConstraints {
+            $0.top.equalTo(footerWeatherDataBtn.snp.top).offset(35)
+            $0.size.equalTo(CGSize(width: 30, height: 30))
+            $0.centerX.equalTo(footerView.snp.centerX)
+        }
+    }
 }
 
 // MARK: UITableViewDelegate
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as! WeatherTableViewCell
-
+        
         return cell
     }
     
