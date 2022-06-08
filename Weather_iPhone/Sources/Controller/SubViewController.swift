d6b791ccc0cd6ec0ad25671f959478c330bac49a
suzumsz <tnwjd6496@naver.com>
2022-06-08 21:48:26 +0900

    [Add] SubViewController 추가 (#21)

---
+++ Weather_iPhone/Sources/Controller/SubViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SubViewController.swift
+//  Weather_iPhone
+//
+//  Created by 김수정 on 2022/06/08.
+//
+
+import UIKit
+import SnapKit
+
+class SubViewController: UIViewController {
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        navigationController?.isNavigationBarHidden = true
+    }
+}
